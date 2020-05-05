f77e77dc8657c98b12669624afaa7bc039e86af3
rubenmorcillo <ravent953@gmail.com>
2020-05-06 01:20:19 +0200

    Movimiento de camara con ratón

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float movementTime;
+
+    public Vector3 newPosition;
+   
+
+    void Update()
+    {
+        HandleMovementInput();
+    }
+
+    void HandleMovementInput()
+    {
+        if (newPosition == null)
+        {
+            newPosition = transform.position;
+        }
+
+        if (Input.mousePosition.y > Screen.height - Screen.height * 0.05)
+        {
+            newPosition += transform.forward * movementSpeed;
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementSpeed);
+        }
+        else if(Input.mousePosition.y < 0 + Screen.height * 0.05)
+        {
+            newPosition += transform.forward * -movementSpeed;
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementSpeed);
+        }
+        
+        if(Input.mousePosition.x > Screen.width - Screen.width * 0.05)
+        {
+            newPosition += transform.right * movementSpeed;
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementSpeed);
+        }
+        else if(Input.mousePosition.x <= 0 - Screen.width * 0.05)
+        {
+            newPosition += transform.right * -movementSpeed;
+            transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementSpeed);
+        }
+
+        newPosition = transform.position;
+    }
+}
